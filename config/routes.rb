eb160e88374ea18d2b0c26b86fffd3384070ec39
kedeinroga <keybroga@gmail.com>
2022-04-18 19:04:45 -0500

    scss gem and redirect correct to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
+  # resources :tasks, only: %i[create new edit update destroy]
   resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
